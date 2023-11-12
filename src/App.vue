f25c6cbd173d90af4a99120b0a4bfee68dfc0d69
Jharby <jsara030@JharbyPC.localdomain>
2023-11-11 23:14:13 -0500

    some front end

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
     <div id="content" class="app-nextopn">
-        <AppNavigation>
-        </AppNavigation>
-        <AppContent>
-          <getCategories/>
-        </AppContent>
+        <a class="active" href="#categories">Categories</a>
+        <a href="#firmware"><i class="fa fa-fw fa-search"></i>Firmware</a>
+        <a href="#aliases">Aliases</a>
+        <a href="#group">Group</a>
+        <a href="#system">System</a>
+        <a href="#user">User</a>
     </div>
 </template>
 
@@ -20,11 +21,17 @@ import getAlias from './components/getAlias.vue'
 import AddGroup from './components/AddGroup.vue'
 import DeleteGroup from './components/DeleteGroup.vue'
 import getCategories from './components/getCategories.vue'
+import 'font-awesome/css/font-awesome.min.css';
 
 export default {
 	name: 'App',
 	components: {
 		getCategories,
+    Firmware,
+    Categories,
+    getAlias,
+    AddGroup,
+    DeleteGroup
 	},
 	data() {
 		return {
@@ -38,6 +45,36 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+.app-nextopn {
+  background-color: #333;
+  overflow: hidden;
+}
+
+.app-nextopn a {
+  font-family: 'Roboto', sans-serif; /* This is a modern font */
+  font-size: 16px; /* Adjust size as needed */
+  font-weight: 700; /* 700 is for bold text */
+  text-transform: uppercase; /* Makes all text uppercase */
+  letter-spacing: 0.05em; /* Adds spacing between letters */
+  color: #ffffff; /* White text is usually modern and contrasts well with darker backgrounds */
+  padding: 14px 16px;
+  text-decoration: none;
+  transition: color 0.3s ease; /* Smooth transition for hover effects */
+}
+
+.app-nextopn a:hover {
+  color: #dddddd; /* Slightly lighter color on hover for a subtle effect */
+  text-decoration: none; /* Ensures that no underline appears on hover */
+}
+
+/* Add a media query for responsive font sizing on smaller screens */
+@media (max-width: 768px) {
+  .app-nextopn a {
+    font-size: 14px; /* Smaller font size on mobile devices */
+  }
+}
+
 
 </style>
